7da99a9afb302a88d89aaadbc7b48df135e42b7b
Aviroop <151872989+MyselfAviroop@users.noreply.github.com>
2025-04-02 13:25:56 +0530

    Update craco.config.js

--- MERN-Blog-App/frontend/craco.config.js
+++ MERN-Blog-App/frontend/craco.config.js
@@ -1,10 +1,18 @@
-module.exports = {
+import path from 'path';
+import { fileURLToPath } from 'url';
+import pathBrowserify from 'path-browserify';
+import osBrowserify from 'os-browserify/browser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   webpack: {
     configure: {
       resolve: {
         fallback: {
-          "path": require.resolve("path-browserify"),
-          "os": require.resolve("os-browserify/browser"),
+          "path": pathBrowserify,
+          "os": osBrowserify,
           "fs": false
         }
       }
